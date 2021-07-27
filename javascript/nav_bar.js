aca0be8f72dc729e11b695de38e315a52e13276b
Thomas Cooper <thomascooper@T-JR-MBP.fios-router.home>
2021-07-26 21:50:50 -0400

    Revamp entire website.

---
+++ javascript/nav_bar.js
@@ -0,0 +1,23 @@
+const hamburgerMenu = document.getElementById("hamburger-icon");
+const navigation = document.getElementById("navigation");
+
+// Open mobile nav menu on hamburger button click
+hamburgerMenu.addEventListener("click", function () {
+	navigation.style.paddingBottom = "100%";
+});
+
+/* If clicking outside of the nav menu, and if the nav menu is already open, close the mobile nav menu */
+document.addEventListener("click", function () {
+	if (navigation.clientHeight != "0") {
+		navigation.style.paddingBottom = "0%";
+	}
+});
+
+/* If window grows larger than 700px wide, hide mobile nav dropdown menu */
+function setBottomPaddingToZero() {
+	if (window.innerWidth > 700) {
+		navigation.style.paddingBottom = "0%";
+	}
+}
+
+window.onresize = setBottomPaddingToZero;
