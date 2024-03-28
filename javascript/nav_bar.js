66a693c831a576a7e63d3600791561be23cab70b
Thomas Cooper <thomascooper@Thomass-MacBook-Pro.local>
2024-03-27 20:16:04 -0400

    Remove the screen behind the mobile nav when the browser window expands.

--- javascript/nav_bar.js
+++ javascript/nav_bar.js
@@ -22,6 +22,7 @@ document.addEventListener("click", () => {
 function setBottomPaddingToZero() {
 	if (window.innerWidth > 700) {
 		navigation.style.paddingBottom = "0%";
+		screenBehindMobileNav.remove();
 	}
 }
 
