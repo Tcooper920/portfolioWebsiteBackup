cecf86385b33c22895e1a9b6ee900b28353ffcbe
Thomas Cooper <thomascooper@Thomass-MBP.fios-router.home>
2025-08-19 20:36:25 -0400

    Update nav for accessibility, remove portfolio pieces, and update popup icons.

--- javascript/nav_bar.js
+++ javascript/nav_bar.js
@@ -1,28 +1,35 @@
 const hamburgerMenu = document.getElementById("hamburger-icon");
 const navigation = document.getElementById("navigation");
+const navigationMenuItems = document.getElementsByClassName("nav-mobile-dropdown")[0];
 const screenBehindMobileNav = document.createElement("div");
 
-screenBehindMobileNav.classList.add("screen_behind_mobile_nav");
+screenBehindMobileNav.classList.add("screen-behind-mobile-nav");
 
-// Open mobile nav menu on hamburger button click
+/* Open mobile nav menu on hamburger button click. */
 hamburgerMenu.addEventListener("click", () => {
-	navigation.style.paddingBottom = "24rem";
+	navigation.className = "expand";
+	navigationMenuItems.classList.remove("hide");
 	navigation.after(screenBehindMobileNav);
 });
 
-/* If clicking outside of the nav menu, and if the nav menu is already open, close the mobile nav menu */
+/* If clicking outside of the nav menu, and if the nav menu is already open, close the mobile nav menu. */
 document.addEventListener("click", () => {
-	if (navigation.clientHeight != "0") {
-		navigation.style.paddingBottom = "0%";
+	if (navigation.clientHeight !== 0 && window.innerWidth < 700) {
+		navigation.className = "collapse";
+		navigationMenuItems.classList.add("hide");
 		screenBehindMobileNav.remove();
 	}
 });
 
-/* If window grows larger than 700px wide, hide mobile nav dropdown menu */
+/* If window grows larger than 700px wide, hide mobile nav dropdown menu. */
 function setBottomPaddingToZero() {
 	if (window.innerWidth > 700) {
-		navigation.style.paddingBottom = "0%";
+		navigation.className = "collapse";
+		navigationMenuItems.classList.remove("hide");
 		screenBehindMobileNav.remove();
+	}
+	if (window.innerWidth <= 700 && navigation.style.paddingBottom === "0%") {
+		navigationMenuItems.classList.add("hide");
 	}
 }
 
