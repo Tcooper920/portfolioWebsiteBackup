135e27d41a93aa2ffa73889d587cafa269dab408
Thomas Cooper <thomascooper@Thomass-MBP.fios-router.home>
2025-10-25 21:58:38 -0400

    Dynamically add contact email to nav to hide from bots. Adjust nav styling.

--- javascript/index.js
+++ javascript/index.js
@@ -2,6 +2,8 @@
 
 import { setupMobileNavigationFunctionality } from "./includes/nav-bar.js";
 import { scrollToTopFunctionality } from "./includes/scroll-to-top-btn.js";
+import { insertEmailIntoNavBar } from "./includes/insert-nav-email.js";
 
 setupMobileNavigationFunctionality();
 scrollToTopFunctionality();
+insertEmailIntoNavBar();
